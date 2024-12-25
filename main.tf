a95e147545004b492261697fe41aee1a54214f86
tak <mailtotakaf@gmail.com>
2024-12-25 22:45:03 +0900

    kc にリネーム

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "lambda_assume_role_policy" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name               = "example-lambda-role"
+  name               = "kc-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
@@ -32,8 +32,8 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution_role" {
 #####################################################
 # DynamoDB テーブル
 #####################################################
-resource "aws_dynamodb_table" "sample_table" {
-  name           = "sample-terraform-table"
+resource "aws_dynamodb_table" "kc_table" {
+  name           = "kc-table"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
 
@@ -54,8 +54,8 @@ data "archive_file" "lambda_zip" {
   output_path = "lambda_function.zip"
 }
 
-resource "aws_lambda_function" "sample_lambda" {
-  function_name = "example-lambda-function"
+resource "aws_lambda_function" "kc_lambda" {
+  function_name = "kc-lambda-function"
   runtime       = "python3.9"
   handler       = "lambda_function.lambda_handler"
   role          = aws_iam_role.lambda_role.arn
@@ -64,7 +64,7 @@ resource "aws_lambda_function" "sample_lambda" {
   # DynamoDB テーブル名を環境変数で渡す例
   environment {
     variables = {
-      TABLE_NAME = aws_dynamodb_table.sample_table.name
+      TABLE_NAME = aws_dynamodb_table.kc_table.name
     }
   }
 }
@@ -74,57 +74,57 @@ resource "aws_lambda_function" "sample_lambda" {
 # - リソース定義、メソッド定義、Lambda 連携
 #####################################################
 # REST API 作成
-resource "aws_api_gateway_rest_api" "sample_api" {
-  name        = "sample-api"
-  description = "Sample API with Lambda & DynamoDB"
+resource "aws_api_gateway_rest_api" "kc_api" {
+  name        = "kc-api"
+  description = "kc API with Lambda & DynamoDB"
 }
 
 # パス /items を作成
-resource "aws_api_gateway_resource" "sample_resource" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  parent_id   = aws_api_gateway_rest_api.sample_api.root_resource_id
+resource "aws_api_gateway_resource" "kc_resource" {
+  rest_api_id = aws_api_gateway_rest_api.kc_api.id
+  parent_id   = aws_api_gateway_rest_api.kc_api.root_resource_id
   path_part   = "items"
 }
 
 # メソッド (GET) を作成 (AUTH 無し)
-resource "aws_api_gateway_method" "sample_method" {
-  rest_api_id   = aws_api_gateway_rest_api.sample_api.id
-  resource_id   = aws_api_gateway_resource.sample_resource.id
+resource "aws_api_gateway_method" "kc_method" {
+  rest_api_id   = aws_api_gateway_rest_api.kc_api.id
+  resource_id   = aws_api_gateway_resource.kc_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 # API Gateway と Lambda を統合 (AWS_PROXY モード)
-resource "aws_api_gateway_integration" "sample_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.sample_api.id
-  resource_id             = aws_api_gateway_resource.sample_resource.id
-  http_method             = aws_api_gateway_method.sample_method.http_method
+resource "aws_api_gateway_integration" "kc_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.kc_api.id
+  resource_id             = aws_api_gateway_resource.kc_resource.id
+  http_method             = aws_api_gateway_method.kc_method.http_method
   integration_http_method = "POST"  # Lambda呼び出し時の method
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.sample_lambda.invoke_arn
+  uri                     = aws_lambda_function.kc_lambda.invoke_arn
 }
 
 # API Gateway から Lambda を呼び出せるようにする権限
 resource "aws_lambda_permission" "apigw_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.sample_lambda.function_name
+  function_name = aws_lambda_function.kc_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   # execution_arn に対してワイルドカード指定
-  source_arn    = "${aws_api_gateway_rest_api.sample_api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.kc_api.execution_arn}/*/*"
 }
 
 # デプロイメント作成
-resource "aws_api_gateway_deployment" "sample_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
+resource "aws_api_gateway_deployment" "kc_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.kc_api.id
   depends_on = [
-    aws_api_gateway_integration.sample_integration,
+    aws_api_gateway_integration.kc_integration,
   ]
 }
 
 # ステージ作成
-resource "aws_api_gateway_stage" "sample_stage" {
-  rest_api_id   = aws_api_gateway_rest_api.sample_api.id
-  deployment_id = aws_api_gateway_deployment.sample_deployment.id
+resource "aws_api_gateway_stage" "kc_stage" {
+  rest_api_id   = aws_api_gateway_rest_api.kc_api.id
+  deployment_id = aws_api_gateway_deployment.kc_deployment.id
   stage_name    = "dev"
 }
