1e2ecb9b2964448965bea826384e694364e24f6f
tak <mailtotakaf@gmail.com>
2025-03-31 13:35:23 +0900

    元のリクエストも復帰

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -12,16 +12,30 @@ def lambda_handler(event, context):
     try:
         body = json.loads(event['body'])
 
-        gameid_moves = body["gameid_moves"]
-        stones_data = body["stones"]
+        if isinstance(body, dict) and 'gameid_moves' in body and 'stones' in body:
+            # パターン1: 全体に gameid_moves と stones が含まれる形式
+            gameid_moves = body["gameid_moves"]
+            stones_data = body["stones"]
 
-        kc_flg = {}
+            kc_flg = {}
 
-        for player, stones in stones_data.items():
-            # 各プレイヤーの石を (x, y) タプルに変換
-            stones_tuple = [(stone["x"], stone["y"]) for stone in stones.values()]
-            # プレイヤーごとにチェック
-            kc_flg[player] = kc_checker.alert_on_square_or_pre_square(stones_tuple)
+            for player, stones in stones_data.items():
+                # 各プレイヤーの石を (x, y) タプルに変換
+                stones_tuple = [(stone["x"], stone["y"]) for stone in stones.values()]
+                # プレイヤーごとにチェック
+                kc_flg[player] = kc_checker.alert_on_square_or_pre_square(stones_tuple)
+        
+        else:
+            # パターン2: stones のみが直接来る形式
+            stones = body
+            gameid_moves = None
+            stones_data = None
+
+            print("stones:", stones)
+            stones_tuple = [(item["x"], item["y"]) for i, (key, item) in enumerate(stones.items())]
+            print("stones_tuple:", stones_tuple)
+
+            kc_flg = kc_checker.alert_on_square_or_pre_square(stones_tuple)
 
     except (KeyError, json.JSONDecodeError) as e:
         print(f"KeyError or JSONDecodeError: {str(e)}")
